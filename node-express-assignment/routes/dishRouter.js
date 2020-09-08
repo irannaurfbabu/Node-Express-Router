154fd1af8ca72e0f13cf0e20957a5d92257863ec
Iranna Nashi <irannaurfbabu@gmail.com>
2020-09-08 23:31:26 +0530

    updated with route methods for dishRouter

--- node-express-assignment/routes/dishRouter.js
+++ node-express-assignment/routes/dishRouter.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const bodyParser = require('body-parser');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var dishRouter = express.Router();
+
+dishRouter.route('/').all((req, res, next) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  next();
+})
+.get((req, res, next) => {
+  res.end('Will send all dishes to you!');
+})
+.post((req, res, next) => {
+  res.end('Will add the dish: '+ req.body.name + 'with details: ' + req.body.description);
+})
+.put((req, res, next) => {
+  res.statusCode = 403;
+  res.end('PUT operation not supported on /dishes');
+})
+.delete((req, res, next) => {
+  res.end('Deleting all dishes');
 });
 
-module.exports = router;
+dishRouter.route('/:dishId').get((req, res, next) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Will send details of dish: ' + req.params.dishId + 'to you!');
+});
+
+module.exports = dishRouter;
