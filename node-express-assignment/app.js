8734dcf3d2ae024f46e4d672b03b8d27292d3f9c
Iranna Nashi <irannaurfbabu@gmail.com>
2020-09-08 23:10:16 +0530

    added public files and updated router files

--- node-express-assignment/app.js
+++ node-express-assignment/app.js
@@ -3,9 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const http = require('http');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var dishRouter = require('./routes/dishRouter');
+var leadersRouter = require('./routes/leaderRouter');
+
+const hostname = 'localhost';
+const port = 3000;
 
 var app = express();
 
@@ -15,12 +19,9 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/dishes', dishRouter);
+app.use('/users', leadersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +38,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+const server = http.createServer(app);
 
-module.exports = app;
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
+});
