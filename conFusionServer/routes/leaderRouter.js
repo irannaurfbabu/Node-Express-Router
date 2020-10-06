046beedbb8909cac56728506e8cb69ea3a8f53ee
iranna <irannaurfbabu@gmail.com>
2020-10-06 19:19:58 +0530

    Added conFusionServer for mongodb

---
+++ conFusionServer/routes/leaderRouter.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var leaderRouter = express.Router();
+
+leaderRouter.route('/').all((req, res, next) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  next();
+})
+.get((req, res, next) => {
+  res.end('Will send all leaders details to you!');
+})
+.post((req, res, next) => {
+  res.end('Will add the leader: '+ req.body.name + 'with details: ' + req.body.description);
+})
+.put((req, res, next) => {
+  res.statusCode = 403;
+  res.end('PUT operation not supported on /dishes');
+})
+.delete((req, res, next) => {
+  res.end('Deleting all leaders details');
+});
+
+leaderRouter.route('/:leaderId').get((req, res, next) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Will send details of leader: ' + req.params.leaderId + 'to you!');
+});
+
+module.exports = leaderRouter;
